03ffd127cf325ba8c8e59ed9673daf891c222b6a
Narankhuu <53722454+narankhuu746@users.noreply.github.com>
2022-12-04 18:26:32 +0800

    Add files via upload

---
+++ JS/tour.js
@@ -0,0 +1,75 @@
+[
+    {
+        tour_name : "Scenes of National Parks",
+        tour_img : "/PICTURES/tour1.webp",
+        tour_master : [
+            "3 Days 2 Nights",
+            "Exploring varied natural landscapes",
+            "Visiting cultural heritage sites",
+            "Experiencing nomad culture",
+            "Hiking"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    },
+    {
+        tour_name : "Contrasts of Mongolia",
+        tour_img : "/PICTURES/tour3.webp",
+        tour_master : [
+            "8 Days 7 Nights",
+            "Vast & diverse Gobi desert",
+            "Taiga & Khuvsgul Lake",
+            "Wildlife Watching",
+            "Luxury accommodation"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    },
+    {
+        tour_name : "On Horseback to Mystical Taiga",
+        tour_img : "/PICTURES/tour2.webp",
+        tour_master : [
+            "12 Days 11 Nights",
+            "Explore the Mystical Taiga",
+            "Visit Reindeer & Khuvsgul lake",
+            "Stay with Tsaachin family",
+            "Luxury accommodation"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    },
+    {
+        tour_name : "The Heritage of the Sacred Land",
+        tour_img : "/PICTURES/tour3.webp",
+        tour_master : [
+            "6 Days 5 Nights",
+            "Enjoy Chinggis Khaan statue",
+            "Visit cultural heritage",
+            "Spotting wild sheep<",
+            "Explore historical site"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    },
+    {
+        tour_name : "Treasures of Mongolia",
+        tour_img : "/PICTURES/tour1.webp",
+        tour_master : [
+            "12 Days 11 Nights",
+            "Explore the Gobi Desert",
+            "Visit Orkhon Waterfall",
+            "Stay with yak herders",
+            "Family fun"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    },
+    {
+        tour_name : "Snow Leopard Quest",
+        tour_img : "/PICTURES/tour2.webp",
+        tour_master : [
+            "12 Days 11 Nights",
+            "Visit ethnic groups",
+            "Spotting wild cats",
+            "Hiking through high mountains",
+            "Experiencing unique nomadic life"
+        ] ,
+        tour_info_link : "/HTML/tour_info.html"
+    }
+    
+];
